58d16310df41fcaad0741ffa1c6b6b8be5aeb623
Alice <aliceladybunny@gmail.com>
2024-05-20 20:59:13 +0200

    hola usa la clase HolaMundo

--- lib/archivo.php
+++ lib/archivo.php
@@ -1,5 +1,7 @@
 <?php
 // Autor: Alice Gonzalez <aliceladybunny@gmail.com>
 // El nombre por defecto es Mundo
+require('HolaMundo.php');
+
 $nombre = isset($argv[1]) ? $argv[1] : "Mundo";
-@print "Hola, {$nombre}\n";
+print new HolaMundo($nombre);
